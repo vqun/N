c0bff9df2a1de8afc500d83767d2228a2c2fc4cc
zengweiqun <zengweiqun@meituan.com>
2017-05-08 00:29:22 +0800

    webpack init OK

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,44 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
+  context: path.resolve(__dirname, 'pages'),
   entry: produceEntries('pages'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: 'assets/[name].bundle.js'
   },
   module: {
     rules: [
-      {test: /\.(js)$/, use: 'babel-loader'}
+      {
+        test: /\.js/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                "targets": {
+                  "browsers": ["last 5 versions"]
+                }
+              }]
+            ]
+          }
+        }]
+      }
     ]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({template: './src/index.html'})
-  ],
-  babel: {
-    presets: [
-      [
-        "env",
-        {
-          "targets": {
-            "browsers": ["last 5 versions"]
-          }
-        }
-      ]
-    ]
-  }
+    new HtmlWebpackPlugin()
+  ]
 };
 
 module.exports = config;
 
-function produceEntries(context) {
-  const files = fs.readdirSync(path.resolve(__dirname, context));
+function produceEntries(context, prefix = '') {
+  const base = path.resolve(__dirname, context), files = fs.readdirSync(base);
   return files.reduce((prev, file) => {
-    const stat = fs.statSync(file)
-    if(stat.isFile) return prev[file] = file;
-    // TODO: 将/转换为.
-    if(stat.isDirectory) return Object.assign(prev, produceEntries(file))
+    const _file = path.resolve(base, file), name = `${prefix ? `${prefix}/` : ''}${file}`.replace(/\.js$/, ''), stat = fs.statSync(_file);
+    if(stat.isFile()) return Object.assign({}, prev, { [name]: `./${name}` });
+    if(stat.isDirectory()) return Object.assign(prev, produceEntries(_file, name))
   }, {})
 }
